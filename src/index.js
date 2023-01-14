b7be40e1ee0286126122484a492611e5227076da
Ivan Malenko <ivan.malenko18@gmail.com>
2023-01-14 12:15:10 +0200

    add bkg

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+
+const url = `api.openweathermap.org/data/2.5/forecast?appid=4ef0639981daea9d7a2bed8d621b5b88&units=metric&q=kyiv&cnt=1`
+
+
