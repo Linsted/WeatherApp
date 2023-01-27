8eb348e4716e6ab16c93c0b45851dcd31a813f9b
Ivan Malenko <ivan.malenko18@gmail.com>
2023-01-28 00:29:53 +0200

    add butons, loader and local storage

--- src/index.js
+++ src/index.js
@@ -1,41 +1,81 @@
 
-const url = `api.openweathermap.org/data/2.5/forecast?appid=4ef0639981daea9d7a2bed8d621b5b88&units=metric&q=kyiv&cnt=1`
 import Notiflix from 'notiflix';
 
 
-console.log(1);
+console.log(2);
 
 
 const refs = {
     form: document.querySelector(`.weather__form`),
     ul: document.querySelector(`.weather__list`),
+    cityList: document.querySelector(`.city__list`),
+    input: document.querySelector(`.weather__input`),
+    button: document.querySelector(`.city__button`),
+    loader: document.querySelector(`.loader`),
+
 }
 let markUp = ``;
+refs.input.value = localStorage.getItem(`city`);
 
-refs.form.addEventListener('submit', onSubmit)
+// refs.loader.hidden = false;
+
+
+function addToLocalStorage(city) {
+    localStorage.setItem("city", `${city}`);
+}
+
+refs.form.addEventListener('submit', onSubmit);
+refs.cityList.addEventListener(`click`, onClick);
+
+
+function onClick(evt) {
+    refs.ul.innerHTML = ``;
+    refs.loader.hidden = false;
+    console.log(evt.target.nodeName);
+    const city = evt.target.textContent;
+    if (evt.target.nodeName !== `BUTTON`) return;
+    refs.input.value = city;
+    
+    fetchForecast(city)
+        .then(data => {
+            refs.loader.hidden = true;
+            addToLocalStorage(city);
+            markUpWeather(data);
+        })
+        .catch(console.log);
+
+
+    
+}
 
 function onSubmit(evt) {
-    evt.preventDefault()
+    evt.preventDefault();
+    
     refs.ul.innerHTML = ``
     const cityName = evt.currentTarget.elements.input.value.trim();
     if (!cityName) {Notiflix.Notify.warning('Enter your city, please.')
-        return
-    };
-
+        return};
+    refs.loader.hidden = false;
+    
     fetchForecast(cityName)
+        .then(data => {
+            refs.loader.hidden = true;
+            addToLocalStorage(cityName);
+            markUpWeather(data);
+        })
+        .catch(console.log);
 }
 
 function fetchForecast(city) {
 
     return fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=4ef0639981daea9d7a2bed8d621b5b88`)
     .then(response => {
-            if (!response.ok) {Notiflix.Notify.warning('We can`t find this city')}
+        if (!response.ok) {
+            Notiflix.Notify.warning('We can`t find this city.');
+            refs.loader.hidden = true;
+            throw new Error(error.message)}
             return response.json()})
-    .then(data => {markUpWeather(data)})
-        .catch(error => {
-            console.log(error)
-            Notiflix.Notify.warning('Sorry, an error ocured')
-        })
+    
 }
 
 
